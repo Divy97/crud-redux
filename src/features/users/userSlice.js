ae33b8d609c3c84981c23504f3ece2fa4fe1c171
Divy <divyparekh1810@gmail.com>
2022-11-27 23:57:37 +0530

    Done with the website

--- src/features/users/userSlice.js
+++ src/features/users/userSlice.js
@@ -7,7 +7,7 @@ const userSlice = createSlice({
   initialState,
   reducers: {
     addUser: (state, action) => {
-      state.push(action.payload);
+      state.unshift(action.payload);
     },
     editUser: (state, action) => {
       const { id, name, email } = action.payload;
